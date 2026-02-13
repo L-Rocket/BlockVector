6f39fd305e1b9ccff3f31854cc3cbee024ac32bb
lea <weihao.li@duke.edu>
2026-02-13 21:51:29 +0800

    up data main

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 #include <vector>
 
 int main() {
-    std::vector<int> numbers = {1, 2, 3, 4, 5};
-    for (int number : numbers) {
-        std::cout << number << std::endl;
+    std::vector<int> numbers;
+    for (int i = 0; i < 10; ++i) {
+        numbers.push_back(i);
+        if (i > 1024) {
+            std::cout << "Added: " << i << ", size: " << numbers.size() << ", capacity: " << numbers.capacity() << std::endl;
+        }
+
     }
     return 0;
 }
